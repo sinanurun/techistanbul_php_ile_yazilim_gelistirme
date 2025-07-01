6666425e89e2b1e2bb62f31ee6269e26d162d771
Sinan URUN <sinanurun24@gmail.com>
2025-07-01 17:44:24 +0300

    Create ornek02.php

---
+++ gun01_php_temelleri_ve_veri_yapilari/ornek02.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Merhaba Dünya";
+echo "Birinci", "İkinci", "üçüncü"; //çoklu parametre
+echo("dönrüncü"."beşinci");
+echo "altıncı"."yedinci";
+
+print "<br> bu print ile yazılmıştır<br>";
+
+
+?>
